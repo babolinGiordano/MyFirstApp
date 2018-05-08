9d9efb42bdd4546b5062fdef249749ee0abe6ed3
Giordano Babolin <giordano.babolin@skylab-group.com>
2018-05-08 11:17:14 +0200

    esercizio

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  selector: 'app-servers',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  /* ----- */
+  username: string;
+
+  onResetUsername() {
+    this.username = '';
+  }
+  /* ----- */
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Served was created! Name is ' + this.serverName;
+  }
+
+
+
+}
